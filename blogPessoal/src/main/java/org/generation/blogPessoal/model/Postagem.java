8587347bb36a1c0a75366d141e509d3f8a39bcfb
luis fernando <fer.almeidabr@hotmail.com>
2021-11-04 07:57:17 -0300

    continuação do blog pessoal, tabela tema

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,61 +6,76 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_postagem")
 public class Postagem {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private long id;
-		
-		@NotNull
-		@Size(min = 5, max = 150)
-		private String titulo;
-		
-		@NotNull
-		@Size(min = 15, max = 450)
-		private String texto;
-		
-		@Temporal(TemporalType.TIMESTAMP)
-		private Date date = new java.sql.Date(System.currentTimeMillis());
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 
-		public long getId() {
-			return id;
-		}
+	@NotNull
+	@Size(min = 5, max = 150, message = "O texto deve ter no minimo 5 caracteres")
+	private String titulo;
 
-		public void setId(long id) {
-			this.id = id;
-		}
+	@NotNull
+	@Size(min = 15, max = 450, message = "O texto deve ter no minimo 15 caracteres")
+	private String texto;
 
-		public String getTitulo() {
-			return titulo;
-		}
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
 
-		public void setTitulo(String tituloString) {
-			this.titulo = tituloString;
-		}
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	public long getId() {
+		return id;
+	}
 
-		public String getTexto() {
-			return texto;
-		}
+	public void setId(long id) {
+		this.id = id;
+	}
 
-		public void setTexto(String texto) {
-			this.texto = texto;
-		}
+	public String getTitulo() {
+		return titulo;
+	}
 
-		public Date getDate() {
-			return date;
-		}
+	public void setTitulo(String tituloString) {
+		this.titulo = tituloString;
+	}
 
-		public void setDate(Date date) {
-			this.date = date;
-		}
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
